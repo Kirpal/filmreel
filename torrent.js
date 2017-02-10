542f126da54e36f5f7c1776e3707eac8b66a9149
Kirpal <kjdemian@gmail.com>
2017-02-09 20:15:12 -0500

    Fixed typo in download code

--- torrent.js
+++ torrent.js
@@ -60,7 +60,7 @@ module.exports = function(movie, download, win, showProgressBar){
       opts.path = path.join(library, movie.id.toString());
       if(!fs.existsSync(opts.path)){
         opts.path.split(path.sep).forEach(function(dir, idx, full){
-          var parent = full.splice(0, idx).join(path.sep);
+          var parent = full.slice(0, idx).join(path.sep);
           var dirPath = path.join(parent, dir);
           if(!fs.existsSync(dirPath)){
             fs.mkdirSync(dirPath);
