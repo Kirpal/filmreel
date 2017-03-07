6769228ed2d53fba7b2ad8d3fa924c79e2e0fafb
Kirpal <kjdemian@gmail.com>
2017-03-06 21:01:27 -0500

    Fixed bug when adding additional settings

--- storeConfig.js
+++ storeConfig.js
@@ -2,30 +2,44 @@
 var fs = require("fs"),
   path = require("path"),
   userData = require("electron").app.getPath("userData"),
-  settings;
+  settings,
+  template = {
+    "port": {
+        "name": "Remote Control Port",
+        "type": "number",
+        "value": 3000
+    },
+    "library": {
+        "name": "Download Location",
+        "type": "directory",
+        "value": path.join(userData, "movies")
+    },
+    "notify": {
+        "name": "Notifications",
+        "type": "boolean",
+        "value": true
+    },
+    "updates": {
+        "name": "Auto Updates",
+        "type": "boolean",
+        "value": true
+    }
+};
+
 try {
   settings = JSON.parse(fs.readFileSync(path.join(userData, "config.json")));
 }catch(err){
   if(err.code === "ENOENT"){
-    settings = {
-        "port": {
-            "name": "Remote Control Port",
-            "type": "number",
-            "value": 3000
-        },
-        "library": {
-            "name": "Download Location",
-            "type": "directory",
-            "value": path.join(userData, "movies")
-        },
-        "notify": {
-            "name": "Notifications",
-            "type": "boolean",
-            "value": true
-        }
-    };
+    settings = template;
   }
 }
+
+Object.keys(template).filter(function(key){
+  return (Object.keys(settings).indexOf(key) === -1);
+}).forEach(function(key){
+  settings[key] = template[key];
+})
+
 
 //store and retreive settings
 
